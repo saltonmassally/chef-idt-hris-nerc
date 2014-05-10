1f735de5d5f47d88cfee1557a01bae9f112c525c
Salton Massally <salton.massally@gmail.com>
2014-05-10 17:17:04 +0000

    initial commit

---
+++ openerp/metadata.rb
@@ -0,0 +1,19 @@
+name             'openerp'
+maintainer       'iDT Labs'
+maintainer_email 'salton.massally@gmail.com'
+license           "Apache 2.0"
+description      'Installs/Configures openerp-server on opswork ... assumes you already have a database running... we use rds'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
+depends "openoffice"
+depends "java"
+depends "supervisor"
+depends "gunicorn"
+depends "nginx"
+
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
