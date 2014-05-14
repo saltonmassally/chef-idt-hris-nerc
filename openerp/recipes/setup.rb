6831805edbebc9d0e2ec65163d3ff58e4c07089f
Salton Massally <salton.massally@gmail.com>
2014-05-14 09:29:26 +0000

    reverting previous

--- openerp/recipes/setup.rb
+++ openerp/recipes/setup.rb
@@ -6,6 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "supervisor"
+include_recipe "gunicorn"
+include_recipe "nginx::repo"
+include_recipe "nginx"
+include_recipe "nginx::http_stub_status_module"
 
 include_recipe 'postgresql::client'
 
